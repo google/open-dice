5e42debe6f248b721986f766ea8d18ffaadb9c67
Rob Mohr <mohrr@google.com>
2023-06-16 22:17:51 +0000

    Fix copy-paste issues
    Bug: b/287529705
    Change-Id: Ic27ae09e2206eef75aae3aec30ee3775cb2ecec9
    Reviewed-on: https://pigweed-review.googlesource.com/c/open-dice/+/152291
    Pigweed-Auto-Submit: Rob Mohr <mohrr@google.com>
    Reviewed-by: Darren Krahn <dkrahn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed.google.com.iam.gserviceaccount.com>

--- tools/setup.py
+++ tools/setup.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The Pigweed Authors
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -11,7 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-"""Utilities for Sample Project development."""
+"""Utilities for Open Dice development."""
 
 import setuptools  # type: ignore
 
